0b61e24c0f9c679e14a80ea1a8d5f0c1c2a92978
Dawid Rautenbach <dawid@rautenbach.za.net>
2019-06-07 21:18:08 +0100

    Split git repository (Moved docs to separate repository)

--- install.bat
+++ install.bat
@@ -1,12 +1,12 @@
 rem call link /u
 cmd /c "npm install"
-cmd /c "cd app & npm install"
-cmd /c "cd jst & npm install"
-cmd /c "cd jst-react & npm install"
-cmd /c "cd jst-preact & npm install"
+cmd /c "cd core & npm install"
+cmd /c "cd webapp & npm install"
 cmd /c "cd rollup-plugin-jst & npm install"
-cmd /c "cd test & npm install"
 cmd /c "cd webpack-plugin-jst & npm install"
+cmd /c "cd services-ui-react & npm install"
+cmd /c "cd services-ui-preact & npm install"
+cmd /c "cd app & npm install"
 cmd /c "cd local-cdn & npm install"
 cmd /c "cd tests & npm install"
 rem call link /i
